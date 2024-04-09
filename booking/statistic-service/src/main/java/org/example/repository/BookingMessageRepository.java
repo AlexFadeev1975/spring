9ac602850c80e03f8c48a55cae1a6b478737a89e
Алексей Фадеев <alex.fadeev43@outlook.com>
2024-04-09 08:06:21 +0300

    to monoservice final

--- booking/statistic-service/src/main/java/org/example/repository/BookingMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.model.kafka.BookingMessage;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookingMessageRepository extends ReactiveMongoRepository<BookingMessage, String> {
-}
--- booking/statistic-service/src/main/java/org/example/repository/RegMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repository;
-
-import org.example.model.kafka.RegMessage;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegMessageRepository extends ReactiveMongoRepository<RegMessage, String> {
-}
